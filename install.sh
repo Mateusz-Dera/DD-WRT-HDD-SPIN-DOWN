2fdfc17d02ce9c2b4bd3bdda2c29d0f80bebae36
Mateusz Dera <34209316+Mateusz-Dera@users.noreply.github.com>
2019-05-05 19:12:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,61 +27,19 @@ echo -e "\e[92;1;48;5;241m |  \e[94;1;48;5;241mMateusz Dera  \e[92;1;48;5;241m |
 echo -e "\e[92;1;48;5;240m | \e[94;1;48;5;240m Version:\e[92;1;48;5;240m 1.0   | \e[0m"
 echo -e "\e[92;1;48;5;239m =================== \e[0m"
 
-echo
-
-cd /jffs || exit 1
-
-if ! [ -d "/jffs/opt" ]; then
-    mkdir /jffs/opt || exit 2
+if ! [ -d "/jffs/.tmp" ]; then
+   mkdir /jffs/.tmp || exit 1
 fi
+cd /jffs/.tmp || exit 1
+curl -kLO https://raw.githubusercontent.com/Mateusz-Dera/DD-WRT-Easy-Optware-ng-Installer/master/install.sh || exit 1
+sh ./install.sh -s 
+/opt/bin/ipkg update || exit 1
+rm -R /jffs/.tmp || exit 1
 
-if ! [ -d "/jffs/etc" ]; then
-    mkdir /jffs/etc || exit 3
-fi
+cd /jffs/opt || exit 1
+/opt/bin/ipkg install sdparm || exit 1
 
-if ! [ -d "/jffs/etc/config" ]; then
-    mkdir /jffs/etc/config || exit 4
-fi
-
-mount -o bind /jffs/opt /opt || exit 5
-
-if ! [ -x "$(command -v /opt/bin/ipkg update)" ]; then
-    echo -e "\e[92;1;48;5;239m =========================== \e[0m"
-    echo -e "\e[92;1;48;5;240m |  INSTALLING OPTWARE-NG  | \e[0m"
-    echo -e "\e[92;1;48;5;241m |  \e[94;1;48;5;241mSelect architecture \e[92;1;48;5;241m   | \e[0m"
-    echo -e "\e[92;1;48;5;242m | \e[94;1;48;5;242m 0\e[92;1;48;5;242m ARMv7 EABI hardfloat | \e[0m"
-    echo -e "\e[92;1;48;5;243m | \e[94;1;48;5;243m 1\e[92;1;48;5;243m ARMv7 EABI softfloat | \e[0m"
-    echo -e "\e[92;1;48;5;244m | \e[94;1;48;5;244m 2\e[92;1;48;5;244m ARMv5 EABI           | \e[0m"
-    echo -e "\e[92;1;48;5;245m | \e[94;1;48;5;245m 3\e[92;1;48;5;245m ARMv5 EABI legacy    | \e[0m"
-    echo -e "\e[92;1;48;5;244m | \e[94;1;48;5;244m 4\e[92;1;48;5;244m MIPSEL               | \e[0m"
-    echo -e "\e[92;1;48;5;243m | \e[94;1;48;5;243m 5\e[92;1;48;5;243m PowerPC 603e         | \e[0m"
-    echo -e "\e[92;1;48;5;242m | \e[94;1;48;5;242m 6\e[92;1;48;5;242m PowerPC e500v2       | \e[0m"
-    echo -e "\e[92;1;48;5;241m | \e[94;1;48;5;241m 7\e[92;1;48;5;241m PowerPC I686         | \e[0m"
-    echo -e "\e[92;1;48;5;240m | \e[94;1;48;5;240m 8\e[92;1;48;5;240m PowerPC x86_64       | \e[0m"
-    echo -e "\e[92;1;48;5;239m =========================== \e[0m\n"
-    read -p $'Number (Default 0): ' arch
-
-    case $arch in
-    "1") link="http://ipkg.nslu2-linux.org/optware-ng/bootstrap/buildroot-armeabi-ng-bootstrap.sh" ;;
-    "2") link="http://ipkg.nslu2-linux.org/optware-ng/bootstrap/buildroot-armv5eabi-ng-bootstrap.sh" ;;
-    "3") link="http://ipkg.nslu2-linux.org/optware-ng/bootstrap/buildroot-armv5eabi-ng-legacy-bootstrap.sh" ;;
-    "4") link="http://ipkg.nslu2-linux.org/optware-ng/bootstrap/buildroot-mipsel-ng-bootstrap.sh" ;;
-    "5") link="http://ipkg.nslu2-linux.org/optware-ng/bootstrap/buildroot-ppc-603e-bootstrap.sh" ;;
-    "6") link="http://ipkg.nslu2-linux.org/optware-ng/bootstrap/ct-ng-ppc-e500v2-bootstrap.sh" ;;
-    "7") link="http://ipkg.nslu2-linux.org/optware-ng/bootstrap/buildroot-i686-bootstrap.sh" ;;
-    "8") link="http://ipkg.nslu2-linux.org/optware-ng/bootstrap/buildroot-x86_64-bootstrap.sh" ;;
-    *) link="http://ipkg.nslu2-linux.org/optware-ng/bootstrap/buildroot-armeabihf-bootstrap.sh"
-    esac
-    
-    wget -O - $link | sh || exit 6
-
-fi
-
-export PATH=$PATH:/opt/bin:/opt/sbin || exit 7
-/opt/bin/ipkg update || exit 8
-/opt/bin/ipkg install sdparm || exit 9
-
-cd /jffs/etc/config/ || exit 10
+cd /jffs/etc/config/ || exit 1
 
 time=18000
 device="/dev/sdb"
@@ -94,8 +52,8 @@ read -p $'Device (Default /dev/sdb): ' read_device
 
 [ -f ./hdd_spin_down.startup ] && rm ./hdd_spin_down.startup
 
-echo -e "#!/bin/sh\nsdparm --flexible -6 -l --set SCT=$time $device\nsdparm --flexible -6 -l --set STANDBY=1 $device" > hdd_spin_down.startup || exit 11
-chmod 700 hdd_spin_down.startup || exit 12
+echo -e "#!/bin/sh\nsdparm --flexible -6 -l --set SCT=$time $device\nsdparm --flexible -6 -l --set STANDBY=1 $device" > hdd_spin_down.startup || exit 1
+chmod 700 hdd_spin_down.startup || exit 1
 
 while true; do
     read -p $'Do you want to reboot your device? (y/n): ' yn
