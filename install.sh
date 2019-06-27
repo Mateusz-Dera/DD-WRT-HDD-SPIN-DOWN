212b08b65f9c263cf026f3a1ff5087715b8dbc8d
Mateusz Dera <34209316+Mateusz-Dera@users.noreply.github.com>
2019-06-27 23:26:04 +0200

    Fix chmod

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@
 echo -e "\e[92;1;48;5;239m =================== \e[0m"
 echo -e "\e[92;1;48;5;240m |  HDD SPIN DOWN  | \e[0m"
 echo -e "\e[92;1;48;5;241m |  \e[94;1;48;5;241mMateusz Dera  \e[92;1;48;5;241m | \e[0m"
-echo -e "\e[92;1;48;5;240m | \e[94;1;48;5;240m Version:\e[92;1;48;5;240m 1.1   | \e[0m"
+echo -e "\e[92;1;48;5;240m | \e[94;1;48;5;240m Version:\e[92;1;48;5;240m 1.2   | \e[0m"
 echo -e "\e[92;1;48;5;239m =================== \e[0m"
 
 if ! [ -d "/jffs/.tmp" ]; then
@@ -55,5 +55,6 @@ read -p $'Device (Default /dev/sdb): ' read_device
 echo -e '#!/bin/sh\n/usr/bin/logger -t START_$(basename $0) "started [$@]"\nSCRLOG=/tmp/$(basename $0).log\ntouch $SCRLOG\nTIME=$(date +"%Y-%m-%d %H:%M:%S")\necho $TIME "$(basename $0) script started [$@]" >> $SCRLOG'  > hdd_spin_down.startup || exit 1
 echo -e "sdparm --flexible -6 -l --set SCT=$spin_time $device\nsdparm --flexible -6 -l --set STANDBY=1 $device" >> hdd_spin_down.startup || exit 1
 echo -e 'TIME=$(date +"%Y-%m-%d %H:%M:%S")\nif [ "$?" -ne 0 ]\nthen\necho $TIME "Error in script execution! Script: $0" >> $SCRLOG\nelse\necho $TIME "Script execution OK. Script: $0" >> $SCRLOG\nfi\n/usr/bin/logger -t STOP_$(basename $0) "return code $?"\nexit $?' >> hdd_spin_down.startup || exit 1chmod 700 hdd_spin_down.startup || exit 1
+chmod 700 hdd_spin_down.startup || exit 1
 
 echo -e "Installation complete!\nRestart router"
