b9cd1d89a7753f815cb08b798c6044b76ddad5ac
Mateusz Dera <34209316+Mateusz-Dera@users.noreply.github.com>
2019-07-07 13:41:35 +0200

    1.3

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@
 echo -e "\e[92;1;48;5;239m =================== \e[0m"
 echo -e "\e[92;1;48;5;240m |  HDD SPIN DOWN  | \e[0m"
 echo -e "\e[92;1;48;5;241m |  \e[94;1;48;5;241mMateusz Dera  \e[92;1;48;5;241m | \e[0m"
-echo -e "\e[92;1;48;5;240m | \e[94;1;48;5;240m Version:\e[92;1;48;5;240m 1.2   | \e[0m"
+echo -e "\e[92;1;48;5;240m | \e[94;1;48;5;240m Version:\e[92;1;48;5;240m 1.3   | \e[0m"
 echo -e "\e[92;1;48;5;239m =================== \e[0m"
 
 if ! [ -d "/jffs/.tmp" ]; then
@@ -41,20 +41,33 @@ cd /jffs/opt || exit 1
 
 cd /jffs/etc/config/ || exit 1
 
-time=18000
+spin_time=18000
 device="/dev/sdb"
 
 read -p $'Spin-down time (Default 18000): ' read_time
-[ -z "$read_time" ] && echo "18000" || time=$read_time
+[ -z "$read_time" ] && echo "18000" || spin_time=$read_time
 
 read -p $'Device (Default /dev/sdb): ' read_device
 [ -z "$read_device" ] && echo "/dev/sdb" || device=$read_device
 
 [ -f ./hdd_spin_down.startup ] && rm ./hdd_spin_down.startup
 
-echo -e '#!/bin/sh\n/usr/bin/logger -t START_$(basename $0) "started [$@]"\nSCRLOG=/tmp/$(basename $0).log\ntouch $SCRLOG\nTIME=$(date +"%Y-%m-%d %H:%M:%S")\necho $TIME "$(basename $0) script started [$@]" >> $SCRLOG'  > hdd_spin_down.startup || exit 1
-echo -e "sdparm --flexible -6 -l --set SCT=$spin_time $device\nsdparm --flexible -6 -l --set STANDBY=1 $device" >> hdd_spin_down.startup || exit 1
-echo -e 'TIME=$(date +"%Y-%m-%d %H:%M:%S")\nif [ "$?" -ne 0 ]\nthen\necho $TIME "Error in script execution! Script: $0" >> $SCRLOG\nelse\necho $TIME "Script execution OK. Script: $0" >> $SCRLOG\nfi\n/usr/bin/logger -t STOP_$(basename $0) "return code $?"\nexit $?' >> hdd_spin_down.startup || exit 1chmod 700 hdd_spin_down.startup || exit 1
-chmod 700 hdd_spin_down.startup || exit 1
+echo -e '#!/bin/sh' > hdd_spin_down.startup || exit 1
+echo -e '/usr/bin/logger -t START_$(basename $0) "started [$@]"' >> hdd_spin_down.startup || exit 1
+echo -e 'SCRLOG=/tmp/$(basename $0).log' >> hdd_spin_down.startup || exit 1
+echo -e 'touch $SCRLOG' >> hdd_spin_down.startup || exit 1
+echo -e 'TIME=$(date +"%Y-%m-%d %H:%M:%S")' >> hdd_spin_down.startup || exit 1
+echo -e 'echo $TIME "$(basename $0) script started [$@]" >> $SCRLOG' >> hdd_spin_down.startup || exit 1
+echo -e "sdparm --flexible -6 -l --set SCT=$spin_time $device" >> hdd_spin_down.startup || exit 1
+echo -e "sdparm --flexible -6 -l --set STANDBY=1 $device" >> hdd_spin_down.startup || exit 1
+echo -e 'TIME=$(date +"%Y-%m-%d %H:%M:%S")' >> hdd_spin_down.startup || exit 1
+echo -e 'if [ "$?" -ne 0 ]' >> hdd_spin_down.startup || exit 1
+echo -e 'then' >> hdd_spin_down.startup || exit 1
+echo -e 'echo $TIME "Error in script execution! Script: $0" >> $SCRLOG' >> hdd_spin_down.startup || exit 1
+echo -e 'else' >> hdd_spin_down.startup || exit 1
+echo -e 'echo $TIME "Script execution OK. Script: $0" >> $SCRLOG' >> hdd_spin_down.startup || exit 1
+echo -e 'fi' >> hdd_spin_down.startup || exit 1
+echo -e '/usr/bin/logger -t STOP_$(basename $0) "return code $?"' >> hdd_spin_down.startup || exit 1
+echo -e 'exit $?' >> hdd_spin_down.startup || exit 1
 
-echo -e "Installation complete!\nRestart router"
+chmod 700 hdd_spin_down.startup || exit 1
